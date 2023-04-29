20a04604299b5ebaea3aefbdb7960001ed0c94b3
faizan ullah kalhoro <40607894+kalhorofaizan@users.noreply.github.com>
2023-04-29 11:37:59 +0500

    Update login.dart

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -97,6 +97,8 @@ class _LoginScreenState extends State<LoginScreen> {
                                     });
                                   });
                                 } else {
+                                  Navigator.pushNamed(
+                                      context, LandingScreen.route);
                                   setState(() {});
                                 }
                               },
